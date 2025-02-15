15f0a442e31f7537a73152d4a2bf0069f7695ca1
Evgeny Zimin <94135768+iamzimin@users.noreply.github.com>
2025-02-15 18:37:05 +0400

    added error preview

--- core/resource/src/main/java/com/evg/ui/PreviewImage.kt
+++ core/resource/src/main/java/com/evg/ui/PreviewImage.kt
@@ -1,20 +1,23 @@
 package com.evg.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
+import com.evg.resource.R
 import com.evg.ui.theme.BorderRadius
-import com.valentinilk.shimmer.shimmer
 
 
 @Composable
@@ -43,23 +46,21 @@ fun PreviewImage(
 
 @Composable
 private fun ImageError() {
-    /*Box(
+    val backgroundColor = if (isSystemInDarkTheme()) Color.Gray else Color.LightGray
+    val iconColor = if (!isSystemInDarkTheme()) Color.Gray else Color.LightGray
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .clip(shape = RoundedCornerShape(BorderRadius))
-            .border(
-                0.5.dp,
-                MaterialTheme.colorScheme.onSurface,
-                RoundedCornerShape(BorderRadius)
-            )
+            .background(backgroundColor),
     ) {
         Icon(
             modifier = Modifier
-                .size(70.dp)
+                .size(40.dp)
                 .align(Alignment.Center),
-            painter = painterResource(id = R.drawable.),
-            contentDescription = "Error",
-            tint =Color.Red,
+            painter = painterResource(id = R.drawable.file_warning),
+            contentDescription = null,
+            tint = iconColor,
         )
-    }*/
+    }
 }
